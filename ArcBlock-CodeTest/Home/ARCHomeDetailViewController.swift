b65555c91be8b38cbb7b976461519b7340213727
GaoYu <gaoyu.dreamer@gmail.com>
2021-02-24 16:57:26 +0800

    build home page workflow

---
+++ ArcBlock-CodeTest/Home/ARCHomeDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ARCHomeDetailViewController.swift
+//  ArcBlock-CodeTest
+//
+//  Created by 高宇 on 2021/2/24.
+//
+
+import UIKit
+
+class ARCHomeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
