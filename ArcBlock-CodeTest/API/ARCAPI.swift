f0690b8f2e8a82cdaaa67934f158a81dc2b2dcb5
GaoYu <gaoyu.dreamer@gmail.com>
2021-02-24 16:13:05 +0800

    add network plugin

--- ArcBlock-CodeTest/API/ARCAPI.swift
+++ ArcBlock-CodeTest/API/ARCAPI.swift
@@ -63,3 +63,34 @@ private extension String {
         return data(using: .utf8)!
     }
 }
+
+
+public enum NetworkActivityChangeType {
+    case began, ended
+}
+
+public final class NetworkActivityPlugin: PluginType {
+
+    public typealias NetworkActivityClosure = (_ change: NetworkActivityChangeType, _ target: TargetType) -> Void
+    let networkActivityClosure: NetworkActivityClosure
+
+    public init(networkActivityClosure: @escaping NetworkActivityClosure) {
+        self.networkActivityClosure = networkActivityClosure
+    }
+
+    public func willSend(_ request: RequestType, target: TargetType) {
+        networkActivityClosure(.began, target)
+    }
+
+    public func didReceive(_ result: Result<Moya.Response, MoyaError>, target: TargetType) {
+        networkActivityClosure(.ended, target)
+    }
+}
+
+
+extension ARCAPI {
+    public static let provider = MoyaProvider<ARCAPI>(plugins: [NetworkActivityPlugin(networkActivityClosure: { (type, targt) in
+  
+    })])
+}
+
