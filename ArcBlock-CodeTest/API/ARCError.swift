44fa7a0c4fe55d6407b0462314b665ccc64ddd97
GaoYu <gaoyu.dreamer@gmail.com>
2021-02-24 16:20:22 +0800

    return parse data

---
+++ ArcBlock-CodeTest/API/ARCError.swift
@@ -0,0 +1,12 @@
+//
+//  ARCError.swift
+//  ArcBlock-CodeTest
+//
+//  Created by 高宇 on 2021/2/24.
+//
+
+import Foundation
+
+enum ARCError: Error {
+    case badRequest
+}
--- ArcBlock-CodeTest/API/Home/ARCHomeAPI.swift
+++ ArcBlock-CodeTest/API/Home/ARCHomeAPI.swift
@@ -10,18 +10,16 @@ import Moya
 import SwiftyJSON
 
 class ARCHomeAPI {
-    func parse() {
-        
-    }
-    
-    func fetchHomeList() {
+    static func fetchHomeList(closure: @escaping (Result<[ARCTimeline], ARCError>) -> Void) {
         let provider = ARCAPI.provider
         provider.request(.fetchHomeList) { (result) in
             switch result {
             case .success(let res):
                 let array = ARCParseData.parseArray(data: res.data, methodName: ARCAPI.fetchHomeList.path, model: ARCTimeline.self)
+                closure(.success(array ?? []))
             case .failure(let error):
-                break
+                ARCLogger.print(message: error.errorDescription)
+                closure(.failure(.badRequest))
             }
         }
     }
