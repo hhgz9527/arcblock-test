bafe23170b7768afa23019c651d0709fbfc66a81
GaoYu <gaoyu.dreamer@gmail.com>
2021-02-24 15:32:03 +0800

    add timeline model

---
+++ ArcBlock-CodeTest/Model/ARCTimeline.swift
@@ -0,0 +1,16 @@
+//
+//  ARCTimeline.swift
+//  ArcBlock-CodeTest
+//
+//  Created by 高宇 on 2021/2/24.
+//
+
+import Foundation
+
+class ARCTimeline: Codable {
+    let id: String?
+    let type: String?
+    let content: String?
+    let imgUrls: [URL?]?
+    let link: String?
+}
